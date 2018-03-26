8de5a42c202a551a8691381cf413f97db106476e
nauris-m <nauris.m@gmail.com>
2018-03-26 15:53:24 +0300

    re-added

---
+++ src/app/movies/movie.service.ts
@@ -0,0 +1,42 @@
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+import {environment} from '../../environments/environment';
+
+import 'rxjs/add/operator/toPromise';
+
+@Injectable()
+export class MovieService {
+
+  private apiUrl = 'https://api.themoviedb.org/3/';
+  private apiKey = 'api_key=' + environment.API_KEY;
+
+  constructor(private http: Http) {
+  }
+
+  getMovie(id: number): Promise<any> {
+    const url = `${this.apiUrl}movie/${id}?${this.apiKey}`;
+    return this.http
+      .get(url)
+      .toPromise()
+      .then((response) => {
+        return response.json() as any;
+      })
+      .catch(this.handleError);
+  }
+
+  searchMovies(name: string): Promise<any> {
+    const url = `${this.apiUrl}search/movie?query=${name}&${this.apiKey}`;
+    return this.http
+      .get(url)
+      .toPromise()
+      .then((response) => {
+        return response.json() as any;
+      })
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    // console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
+  }
+}
